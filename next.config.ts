ba1f3706ac8c7f1ab496447897e9a1e03271b116
Jonas <jonasjanssens123@hotmail.com>
2025-11-18 10:00:37 +0100

    latest mobile

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["http://192.168.0.124:3000"],
-  },
   reactCompiler: true,
 };
 
